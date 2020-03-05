4e01ac77b2b0181925456ce350f7c25b27d0390b
Sarah Gill <gills@uchicago.edu>
2020-03-04 19:44:45 -0600

    Add season to df
    added catagorical varaible season
    1 is winter
    4 is Fall

--- analysis/source/Weekday.py
+++ analysis/source/Weekday.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import datetime 
 
 
 
@@ -9,5 +10,39 @@ df['day_of_week'] = pd.to_datetime(df['date']).dt.dayofweek
 df['weekday'] = 0
 df.loc[df['day_of_week'].isin(list(range(0, 5))), 'weekday'] = 1
 
-export_df(df, PATH, 'merged_plus_weekday.csv')
+#season
+# date in yyyy/mm/dd format
+#Winter (1) begins
+#Tuesday, December 21
+winter = datetime.datetime(2010, 12, 21) 
+#Spring (2) begins
+#Saturday, March 20 2010
+spring = datetime.datetime(2010, 3, 20) 
+#Summer (3) begins
+#Monday, June 21
+summer = datetime.datetime(2010, 6, 21) 
+#Fall (4) begins
+#Wednesday, September 22
+fall = datetime.datetime(2010, 9, 22) 
+
+df['season'] = 0 
+df['date'] = pd.to_datetime(df['date'])
+
+
+
+for i in range(len(df['date'])):
+    if df['date'][i] >= spring and df['date'][i] < summer:
+        df['season'][i] = 2 #spring
+    elif df['date'][i] >= summer and df['date'][i] < fall:
+        df['season'][i] = 3 #summer
+    elif df['date'][i] >= fall and df['date'][i] < winter:
+        df['season'][i] = 4 #fall
+    else:
+        df['season'][i] = 1 #winter
+
+i = 100
+df['date'][100] >= spring 
+
+df.to_csv(os.path.join(PATH, 'merged_plus_weekday_season.csv'))
+
 
